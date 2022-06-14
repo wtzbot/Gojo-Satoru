7eab019fcfb3938296689e37d83a6d264f162a55
wtzbot <96922881+wtzbot@users.noreply.github.com>
2022-06-14 09:04:55 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94769228163']
+global.premium = ['94769228163']
+global.ownernomer = '94769228163'
+global.ownername = '༒☬ꪶ࿋྄ིꫂÇhͥαmͣïͫ†h༻☬༒'
+global.botname = '🆂🅷🅰️🅳🅾️🆆'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = '🆂🅷🅰️🅳🅾️🆆'
+global.author = '༒☬ꪶ࿋྄ིꫂÇhͥαmͣïͫ†h༻☬༒'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
